dd0b8c2fe489050d870959ecda0412386f621a68
Alexander Saavedra <mralexsaavedra@gmail.com>
2024-01-07 19:57:35 +0100

    Improved Carousel component

--- src/components/ui/Carousel.tsx
+++ src/components/ui/Carousel.tsx
@@ -1,18 +1,38 @@
 'use client';
 
-import type { Show } from '@/types';
+import { Fragment, useRef, useState } from 'react';
 import { cn } from '@/lib/utils';
-import { FC, useRef, useState } from 'react';
-import { Card } from '@/components/Card';
 import { ChevronLeft } from '@/components/icons/ChevronLeft';
 import { ChevronRight } from '@/components/icons/ChevronRight';
 
-interface CarouselProps {
-  title: string;
-  shows: Show[];
+interface ClassName {
+  container: string;
+  carousel: string;
 }
 
-const Carousel: FC<CarouselProps> = ({ title, shows }) => {
+interface RenderItemFunctionArgs<T> {
+  readonly item: T;
+  readonly index: number;
+}
+export interface RenderItemFunction<T> {
+  (args: RenderItemFunctionArgs<T>): JSX.Element;
+}
+
+interface CarouselProps<T> {
+  title: string;
+  data: T[];
+  children: RenderItemFunction<T>;
+  onClickTitle?: () => void;
+  className?: Partial<ClassName>;
+}
+
+const Carousel = <T,>({
+  title,
+  data,
+  children,
+  onClickTitle,
+  className,
+}: CarouselProps<T>) => {
   const showsRef = useRef<HTMLDivElement>(null);
   const [isMoved, setIsMoved] = useState(false);
 
@@ -31,12 +51,18 @@ const Carousel: FC<CarouselProps> = ({ title, shows }) => {
   };
 
   return (
-    <div className="space-y-0.5 md:space-y-2">
-      <h2 className="text-lg font-semibold text-white/90 transition-colors hover:text-white sm:text-xl">
+    <div className={cn('space-y-2', className?.container)}>
+      <h2
+        className={cn(
+          'text-lg font-semibold text-white/90 transition-colors sm:text-xl cursor-default',
+          onClickTitle && 'hover:text-white cursor-pointer'
+        )}
+        onClick={onClickTitle}
+      >
         {title}
       </h2>
 
-      <div className="group relative mt-12">
+      <div className="group relative">
         <ChevronLeft
           className={cn(
             'absolute top-0 bottom-0 left-2 z-40 m-auto h-9 w-9 cursor-pointer opacity-0 transition hover:scale-125 group-hover:opacity-100',
@@ -47,16 +73,13 @@ const Carousel: FC<CarouselProps> = ({ title, shows }) => {
 
         <div
           ref={showsRef}
-          className="flex items-center no-scrollbar space-x-0.5 overflow-x-scroll md:space-x-2.5"
+          className={cn(
+            'flex items-center no-scrollbar space-x-0.5 overflow-x-scroll md:space-x-2.5',
+            className?.carousel
+          )}
         >
-          {shows.map((show) => (
-            <Card
-              key={show.id}
-              show={show}
-              path={show.backdrop_path}
-              width={240}
-              height={135}
-            />
+          {data.map((item, index) => (
+            <Fragment key={index}>{children({ index, item })}</Fragment>
           ))}
         </div>
 
